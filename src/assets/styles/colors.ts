aa9af2f2ada7cef88bc4058c86c1ddcfe4806237
Sahil7698 <19bmiit060@gmail.com>
2025-12-12 16:59:11 +0530

    App Icons and common files

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,6 @@
+export const Color = {
+  RED: '#EC1B30',
+  WHITE: '#FFFFFF',
+  BLACK: '#000000',
+  GREY: '#1c2833',
+};
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,5 +2,13 @@ import HomeScreen from './HomeScreen';
 import LoginScreen from './LoginScreen';
 import MovieScreen from './MovieScreen';
 import DemoScreen from './DemoScreen';
-
-export {HomeScreen, LoginScreen, MovieScreen, DemoScreen};
+import ProfileScreen from './ProfileScreen';
+import FeedbackScreen from './FeedbackScreen';
+export {
+  HomeScreen,
+  LoginScreen,
+  MovieScreen,
+  DemoScreen,
+  ProfileScreen,
+  FeedbackScreen,
+};
